9de5bd62f789ec166de507ca89493375aeadf424
Usman Ali <usmanali4073@gmail.com>
2019-07-16 17:17:11 -0400

    Swagger

---
+++ MongoDBWebApi/OpenApiInfo.cs
@@ -0,0 +1,10 @@
+﻿using Swashbuckle.AspNetCore.Swagger;
+
+namespace MongoDBWebApi
+{
+    internal class OpenApiInfo : Info
+    {
+        public string Title { get; set; }
+        public string Version { get; set; }
+    }
+}
--- MongoDBWebApi/Startup.cs
+++ MongoDBWebApi/Startup.cs
@@ -25,7 +25,11 @@ namespace MongoDBWebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,6 +44,17 @@ namespace MongoDBWebApi
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            //Swagger
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
 
             app.UseHttpsRedirection();
             app.UseMvc();
